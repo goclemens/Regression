18b8a6a41803386d3e22e087062370b87bac4310
Clemens Hübner-Worseck <huebner-worseck@zib.de>
2018-05-18 15:24:28 +0200

    fixed calcRegression with basis change

--- src/Regression.js
+++ src/Regression.js
@@ -56,8 +56,14 @@ Regression.prototype.setBasis = function(basisId) {
 
 // trigger the calculation of the regression and save  the estemation parameter and degree of freedom to the object
 Regression.prototype.calcRegression = function(options) {
+
   if (options) {
     Object.assign(this,options);
+  }
+
+  if (options.basis) {
+    this.setBasis(options.basis);
+    delete this.basis;
   }
 
   var output = this.currentBasis.calcRegression({
